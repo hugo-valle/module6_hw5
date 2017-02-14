e62094b7f5798e92fe97a20d52edc3dbe1fdac9b
Hugo Valle <hugovalle1@weber.edu>
2017-02-14 16:38:53 +0000

    Initial commit

---
+++ kaon_hw5_task1.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  kaon_hw5_task1.c
+ *       	Usage:  ./kaon_hw5_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:37:29 PM
+ *       Compiler:  gcc -Wall -Werror 
+ *         Author:  Dr. Hugo Valle (), hugovalle1@weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
